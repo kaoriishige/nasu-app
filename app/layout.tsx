e3435a8220bd25b12b9fc29fa84a3083fcecb163
kaoriishige <adtown001@gmail.com>
2025-06-19 05:08:30 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,23 @@
-import "./globals.css"
-import { Metadata } from "next"
-import { Geist_Sans, Geist_Mono } from "geist/font"
+// /app/layout.tsx
+import './globals.css'
 
-const geistSans = Geist_Sans({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "みんなの那須アプリ",
-  description: "地域密着アプリ55選！初回7日無料で使い放題",
-};
+export const metadata = {
+  title: 'みんなの那須アプリ',
+  description: '地域アプリ55選が使い放題！',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="ja">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
-  );
+  )
 }
+
 
