f42b6e27f5be3e20d46ede9981fe5395005dbe5d
kaoriishige <adtown001@gmail.com>
2025-06-19 19:34:36 +0900

    Update webhook.ts

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,2 +1,15 @@
+// pages/api/webhook.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ ok: true })
+}
 
 
